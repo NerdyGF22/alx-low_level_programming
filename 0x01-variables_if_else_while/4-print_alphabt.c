b7e54e3ec4949c08b73f5cc8e1a68d0a7585d929
Telma Farida <telmafarida.tg@gmail>
2022-07-01 02:29:04 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+/**
+ * main -> prints alphabet in lowercase but skip q and e 
+ * starts a new line
+ * Return : always 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++);
+	{
+		if (ch != 'q' && ch !='e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar(10);
+	return (0);
+}
+
