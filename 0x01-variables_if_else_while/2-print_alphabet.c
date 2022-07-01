7721d3bd5a75209401424828fa152d89a81648ba
Telma Farida <telmafarida.tg@gmail>
2022-07-01 01:52:11 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+# include <stdlib.h>
+# include <time.h>
+#include <stdio.h>
+
+/**
+ *  * main ->  a program that prints the alphabet in lowercase
+ *   * followed by a new line
+ *    * Return : always 0
+ *     */
+ int main(void)
+{
+	int ch;
+	for (ch = 97; ch<=122; ch==)
+	{
+		putcahr(ch);
+	}
+	putchar(10); /*this is an ascii code for a new line
+
+	return(0);
+}
