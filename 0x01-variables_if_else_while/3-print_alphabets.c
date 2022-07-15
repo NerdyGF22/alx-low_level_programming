6be6f773a57a45c720f2453ec769ce09c4aa8135
Telma Farida <telmafarida.tg@gmail>
2022-07-15 02:01:28 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,19 @@
 # include <stdio.h>
 
-/** main -> program that prints uppercase and lower case 
- * and starts a new line
- * Return : always 0
+/**
+ * main -> assign a random number to the variable n each time it is executed
+ * and print the last digit of the number stored in variable n
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int ch;
-	for(ch = 97;ch == 122;ch++)
+
+	for (ch = 97; ch <= 122; ch++)
 	{
 		putchar(ch);
 	}
-	for(ch = 65;ch <= 90;ch++)
-	{
-		putchar(ch);
-	}
-	putchar(10);
+	putchar(10); /*this is an ascii code for a new line*/
 
 	return (0);
 }
