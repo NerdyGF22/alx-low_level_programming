fb15be328d503e41a8fce1ee24efd17084343579
Telma Farida <telmafarida.tg@gmail>
2022-07-22 12:20:02 -0700

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main -> a function to print its name
- * @argc: argc parameter
- * @argv: an array of a command listed
+ * main -> a function to print all arguments
+ * @argc: argument count
+ * @argv: an array of pointer to CLI argurment
  * Return: 0 for success
  */
 in main(int argc, char *argv[])
@@ -11,8 +11,6 @@ in main(int argc, char *argv[])
 	int i;
 
 	for (i = 0; i < argc; i++)
-	{
 		printf("%s\n", argv[i]);
-	}
 	return (0);
 }
