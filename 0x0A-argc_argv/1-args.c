9fa27180406a0bb9259fb76a1a95f9a7be9ead83
Telma Farida <telmafarida.tg@gmail>
2022-07-22 12:04:19 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -> a function that prints its name
+ * @argv: an array of a command
+ * @argc: argc parameter
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n, argc - 1);
+	return (0);
+}
