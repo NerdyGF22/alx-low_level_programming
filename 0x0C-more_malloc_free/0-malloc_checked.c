f727961d2e2defdf66a31eddcc939ad7df1652ab
Telma Farida <telmafarida.tg@gmail>
2022-07-28 05:18:19 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars and initializes with it
+ * create_array - creates an array of chars and initializes it with
  * a specific char.
  * @size: size of an array
  * @c: char to initialize array with
@@ -22,7 +22,9 @@ char *create_array(unsigned int size, char c)
 	}
 
 	arr = malloc(sizeof(char) * size);
+
 	/* check if malloc was succesful */
+
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 void free_grid(int **grid, int height);
 int _putchar(char c);
 char *_strdup(char *str)
@@ -8,4 +5,3 @@ char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
 int **alloc_grid(int width, int height);
 char *argstostr(int ac, char **av);
-#endif
