0fff4831894ecf47e195699294fe1438d2b7cc97
Telma Farida <telmafarida.tg@gmail>
2022-08-01 07:36:23 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,6 +8,7 @@ dog_t *new_dog(char *name, float age, char *owner);
 /**
  * _strlen - Finds the length of a string
  * @str: the string to be measured
+ * Return: the length of the string.
  */
 int _strlen(char *str)
 {
