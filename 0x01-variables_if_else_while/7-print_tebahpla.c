b77aedb524c2958063a0ca3040d4729612c95835
Telma Farida <telmafarida.tg@gmail>
2022-08-02 09:49:31 -0700

    use putchar fot lowercase hexadecimals

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,12 +1,10 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-/**
- *
+/*
  * main -> a program that prints the lowercase alphabet in reverse,
  * followed by a new line
- *
- * Return: always 0
+ * Return: Always 0
  */
 
 int main(void) /* main is an entry that prints lowercase alphabets reversed */
@@ -14,7 +12,7 @@ int main(void) /* main is an entry that prints lowercase alphabets reversed */
 	char ch;
 	int n;
 
-	for (n = 48; n <= 58; n++)
+	for (n = 48; n <= 57; n++)
 	{
 		puchar(n);
 	}
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,10 +1,11 @@
 # include <stdio.h>
 
-/** main -> program that prints all the numbers of base 16 in lowercase
+/**
+ * main -> program that prints all the numbers of base 16 in lowercase
  * strats a new line
  * hexadecimal
  * 0 1 2 3  4 5 6 7 8 9 a b c  d e f
- * Returns : always 0
+ * Return: always 0
  */
 int main(void)
 {
