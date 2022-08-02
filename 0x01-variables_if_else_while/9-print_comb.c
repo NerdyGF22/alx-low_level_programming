c278a4d05a3cf74c987abc1d2cad45da256e3825
Telma Farida <telmafarida.tg@gmail>
2022-08-02 10:26:48 -0700

    prints all possible single numbers with space and comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,24 @@
-# include <stdio.h>
+#include <stdio.h>
 
-/** main -> program that prints all combinations of single digit numbers in ascending order
- * seperated by ,, and space then starts a new line
- * Return : always 0
+/**
+ * main -> program that prints all combinations of single digit
+ * numbers in ascending order
+ * seperated by , and space then starts a new line
+ * Return: always 0
  */
 int main(void)
 {
 	int ch;
+
 	for (ch = 48; ch <= 57; ch++)
 	{
-		putchar(ch)
+		putchar(ch);
 		if (ch != 57)
 		{
 			putchar(44);
 			putchar(32);
 		}
+	}
+	putchar(10);
+	return (0);
 }
